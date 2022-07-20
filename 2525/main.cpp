08b0c351962f7c3d8cd484553332e39330d43548
cherrytips <cherrytips24@gmail.com>
2022-07-20 10:01:36 +0900

    2525번 C++ 추가

---
+++ 2525/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int A, B, C;
+
+  cin >> A >> B;
+  cin >> C;
+
+  B += C;
+
+  if (B >= 60)
+  {
+    int over = B / 60;
+
+    A += over;
+    B %= 60;
+
+    if (A >= 24)
+      A %= 24;
+  }
+
+  cout << A << " " << B;
+
+  return 0;
+}
